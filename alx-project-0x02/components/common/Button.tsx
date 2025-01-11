4a51006774aff26b56bb596842c5633521e75eb8
kabanda.patrick2580 <sendsome2019@gmail.com>
2025-01-11 20:47:48 +0200

    removed the unused React

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ButtonProps } from '@/interfaces';
 
 const Button: React.FC<ButtonProps> = ({ size = 'medium', shape = 'rounded-md', label, onClick, className }) => {
